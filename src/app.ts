46725ed3e66cf349f7c38928e4e84d5456857e73
vladlapkovski <vlad.lapkofffskii@gmail.com>
2023-10-23 14:39:26 +0300

    rotes fix

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,8 @@ const app = express();
 const parserMiddleware = express.json()
 
 app.use(parserMiddleware)
-app.use("/ht_02/api/blogs", blogsRoutes)
-app.use("/ht_02/api/posts", postsRouter)
-app.use("/ht_02/api/testing/all-data", dataRouter)
+app.use("/hometask_03/api/blogs", blogsRoutes)
+app.use("/hometask_03/api/posts", postsRouter)
+app.use("/hometask_03/api/testing/all-data", dataRouter)
 return app;
 }
