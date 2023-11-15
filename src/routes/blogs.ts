f0655a09c236640905a239362e114de68eaa557b
vladlapkovski <vlad.lapkofffskii@gmail.com>
2023-11-15 16:38:37 +0300

    bcv

--- src/routes/blogs.ts
+++ src/routes/blogs.ts
@@ -95,7 +95,7 @@ blogsRoutes.get('/', async (req: Request, res: Response) => {
   const sortDirection = req.query.sortDirection as string || 'desc'; // направление сортировки
   const pageNumber = parseInt(req.query.pageNumber as string) || 1; // номер страницы (по умолчанию 1)
   const pageSize = parseInt(req.query.pageSize as string) || 10; // количество элементов на странице (по умолчанию 10)
-  const startIndex = (pageNumber - 1) * pageSize; // индекс начального элемента
+  const startIndex = (pageNumber) * pageSize; // индекс начального элемента
   const endIndex = pageNumber * pageSize; // индекс конечного элемента
   const blogs = await socialRepository.getBlogs();
   
