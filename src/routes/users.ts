0101f22167514e1b85f69a330911438e63980c28
vladlapkovski <vlad.lapkofffskii@gmail.com>
2023-11-17 05:19:51 +0300

    vd

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -89,7 +89,7 @@ if (typeof password !== "string" || !password || password?.trim()?.length == 0 |
 
 const emailRegex = new RegExp('^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
 
-if (typeof email !== "string" || !email || emailRegex.test(email)) {
+if (typeof email !== "string" || !email || !emailRegex.test(email)) {
     errorsMessages.push({
     message: 'Invalid email', 
     field: "email"
