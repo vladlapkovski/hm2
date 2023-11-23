2cd6b0285a78111e563556c5185efd4895625472
vladlapkovski <vlad.lapkofffskii@gmail.com>
2023-11-23 19:49:31 +0300

    gd

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { blogsRoutes } from "./routes/blogs";
 import { postsRouter } from "./routes/posts";
 import { dataRouter } from "./routes/clear_all_data";
 import { postsForBlogsRoutes } from "./routes/postsForBlogs";
-// import { authRoutes } from "./routes/auth";
+import { authRoutes } from "./routes/auth";
 import { usersRoutes } from "./routes/users";
 
 export const appStart = ()=> {
@@ -16,7 +16,7 @@ app.use("/hometask_05/api/blogs", blogsRoutes)
 app.use("/hometask_05/api", postsForBlogsRoutes)
 app.use("/hometask_05/api/posts", postsRouter)
 app.use("/hometask_05/api/testing/all-data", dataRouter)
-// app.use("/hometask_05/api/auth/login", authRoutes)
+app.use("/hometask_05/api/auth/login", authRoutes)
 app.use("/hometask_05/api/users", usersRoutes)
 return app;
 }
